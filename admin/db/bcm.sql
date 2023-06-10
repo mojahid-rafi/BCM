44503a9756b32f553c09f04cb36ec74b4abd7489
shoyab <abumansurshoyab@gmail.com>
2023-06-10 13:05:01 +0600

    db added

---
+++ admin/db/bcm.sql
@@ -0,0 +1,347 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jun 10, 2023 at 08:47 AM
+-- Server version: 10.4.27-MariaDB
+-- PHP Version: 8.2.0
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `bcm`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `blood_club_info`
+--
+
+CREATE TABLE `blood_club_info` (
+  `ID` int(11) NOT NULL,
+  `Club_Name` varchar(150) NOT NULL,
+  `Founder` varchar(150) NOT NULL,
+  `Phone` varchar(15) NOT NULL,
+  `Email` varchar(50) NOT NULL,
+  `Division` varchar(150) NOT NULL,
+  `District` varchar(50) NOT NULL,
+  `Thana` varchar(50) NOT NULL,
+  `Area` varchar(150) NOT NULL,
+  `Createdat` timestamp NOT NULL DEFAULT current_timestamp(),
+  `Updatedat` datetime DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `blood_donation`
+--
+
+CREATE TABLE `blood_donation` (
+  `ID` int(11) NOT NULL,
+  `Donor_id` int(11) NOT NULL,
+  `Don_date` datetime DEFAULT NULL,
+  `Hospital_Name` varchar(50) NOT NULL,
+  `Address` varchar(150) NOT NULL,
+  `Createdat` timestamp NOT NULL DEFAULT current_timestamp(),
+  `Updatedat` datetime DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `campaign_request`
+--
+
+CREATE TABLE `campaign_request` (
+  `ID` int(11) NOT NULL,
+  `ClubID` int(11) NOT NULL,
+  `Division` varchar(50) NOT NULL,
+  `District` varchar(50) NOT NULL,
+  `Thana` varchar(50) NOT NULL,
+  `LocationType` varchar(50) NOT NULL,
+  `LocName` varchar(50) NOT NULL,
+  `ApxTestCount` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `complaint`
+--
+
+CREATE TABLE `complaint` (
+  `id` int(11) NOT NULL,
+  `Name` varchar(150) NOT NULL,
+  `Subject` varchar(50) NOT NULL,
+  `Description` text NOT NULL,
+  `Feedback` text NOT NULL,
+  `Createdat` timestamp NOT NULL DEFAULT current_timestamp(),
+  `Updatedat` datetime DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `district`
+--
+
+CREATE TABLE `district` (
+  `ID` int(11) NOT NULL,
+  `Division_ID` int(11) NOT NULL,
+  `District_Name` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `division`
+--
+
+CREATE TABLE `division` (
+  `ID` int(11) NOT NULL,
+  `Division_Name` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `donor_info`
+--
+
+CREATE TABLE `donor_info` (
+  `id` int(11) NOT NULL,
+  `Name` varchar(150) NOT NULL,
+  `Gender` varchar(30) NOT NULL,
+  `BloodGroup` varchar(30) NOT NULL,
+  `Dob` varchar(30) NOT NULL,
+  `Division` varchar(30) NOT NULL,
+  `District` varchar(30) NOT NULL,
+  `Thana` varchar(30) NOT NULL,
+  `Area` varchar(50) NOT NULL,
+  `Email` varchar(50) NOT NULL,
+  `Phone` varchar(15) NOT NULL,
+  `Password` varchar(30) NOT NULL,
+  `LastDonationDate` datetime DEFAULT NULL,
+  `Status` varchar(30) NOT NULL,
+  `ClubID` int(11) NOT NULL,
+  `Createdat` timestamp NOT NULL DEFAULT current_timestamp(),
+  `Updatedat` datetime DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `feedback`
+--
+
+CREATE TABLE `feedback` (
+  `id` int(11) NOT NULL,
+  `Name` varchar(150) NOT NULL,
+  `Feedback` text NOT NULL,
+  `Createdat` timestamp NOT NULL DEFAULT current_timestamp(),
+  `Updatedat` datetime DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `request_blood`
+--
+
+CREATE TABLE `request_blood` (
+  `ID` int(11) NOT NULL,
+  `Request_From` varchar(50) NOT NULL,
+  `The_Date` datetime DEFAULT NULL,
+  `Patient_Name` varchar(50) NOT NULL,
+  `Blood_Group` varchar(15) NOT NULL,
+  `No_of_Unit` int(11) NOT NULL,
+  `Donation_Date` datetime DEFAULT NULL,
+  `Days_Left` int(11) NOT NULL DEFAULT 0,
+  `Loc_Info` varchar(150) NOT NULL,
+  `Create_At` timestamp NOT NULL DEFAULT current_timestamp(),
+  `Update_At` datetime DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `thana`
+--
+
+CREATE TABLE `thana` (
+  `ID` int(11) NOT NULL,
+  `District_ID` int(11) NOT NULL,
+  `Name` varchar(150) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `ID` int(11) NOT NULL,
+  `User_Name` varchar(150) NOT NULL,
+  `Email` varchar(50) NOT NULL,
+  `Phone` varchar(15) NOT NULL,
+  `Password` varchar(150) NOT NULL,
+  `Createdat` timestamp NOT NULL DEFAULT current_timestamp(),
+  `Updatedat` datetime DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `blood_club_info`
+--
+ALTER TABLE `blood_club_info`
+  ADD PRIMARY KEY (`ID`);
+
+--
+-- Indexes for table `blood_donation`
+--
+ALTER TABLE `blood_donation`
+  ADD PRIMARY KEY (`ID`);
+
+--
+-- Indexes for table `campaign_request`
+--
+ALTER TABLE `campaign_request`
+  ADD PRIMARY KEY (`ID`);
+
+--
+-- Indexes for table `complaint`
+--
+ALTER TABLE `complaint`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `district`
+--
+ALTER TABLE `district`
+  ADD PRIMARY KEY (`ID`);
+
+--
+-- Indexes for table `division`
+--
+ALTER TABLE `division`
+  ADD PRIMARY KEY (`ID`);
+
+--
+-- Indexes for table `donor_info`
+--
+ALTER TABLE `donor_info`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `feedback`
+--
+ALTER TABLE `feedback`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `request_blood`
+--
+ALTER TABLE `request_blood`
+  ADD PRIMARY KEY (`ID`);
+
+--
+-- Indexes for table `thana`
+--
+ALTER TABLE `thana`
+  ADD PRIMARY KEY (`ID`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`ID`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `blood_club_info`
+--
+ALTER TABLE `blood_club_info`
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `blood_donation`
+--
+ALTER TABLE `blood_donation`
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `campaign_request`
+--
+ALTER TABLE `campaign_request`
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `complaint`
+--
+ALTER TABLE `complaint`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `district`
+--
+ALTER TABLE `district`
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `division`
+--
+ALTER TABLE `division`
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `donor_info`
+--
+ALTER TABLE `donor_info`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `feedback`
+--
+ALTER TABLE `feedback`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `request_blood`
+--
+ALTER TABLE `request_blood`
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `thana`
+--
+ALTER TABLE `thana`
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
